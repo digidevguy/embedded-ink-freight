435dcc13740713df6cde515eea5be52500fdc5d3
Permafun <49506518+Permafun@users.noreply.github.com>
2022-04-05 13:42:27 -0500

    Update to about section, smooth scrolling implemented

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,35 +1,36 @@
-import { Flex, Heading, Stack, Text, VStack } from "@chakra-ui/react";
-import ContactForm from "./ContactForm";
+import { Divider, Flex, Heading, Stack, Text, VStack } from '@chakra-ui/react';
+import ContactForm from './ContactForm';
 
 const ContactSection = ({
-  backgroundColor,
-  color,
-  inputBgColor,
-  inputColor,
-  formColor,
+	backgroundColor,
+	color,
+	inputBgColor,
+	inputColor,
+	formColor,
 }) => {
-  return (
-    <Stack
-      bg={backgroundColor}
-      color={color}
-      p={[4, null, 0]}
-      flexDirection={["column", null, "row"]}
-      justifyContent="space-evenly"
-      align="center"
-      spacing={4}
-    >
-      <VStack textAlign="center">
-        <Heading>GET IN TOUCH</Heading>
-        <Text>500 Terry Francois St San Francisco, CA 94158</Text>
-        <Text>embeddedink22@gmail.com</Text>
-        <Text>4698769438</Text>
-      </VStack>
-      <ContactForm
-        inputBgColor={inputBgColor}
-        inputColor={inputColor}
-        formColor={formColor}
-      />
-    </Stack>
-  );
+	return (
+		<Stack
+			bg={backgroundColor}
+			color={color}
+			p={[4, null, 0]}
+			flexDirection={['column', null, 'row']}
+			justifyContent='space-evenly'
+			align='center'
+			spacing={4}
+		>
+			<VStack textAlign='center'>
+				<Heading>GET IN TOUCH</Heading>
+				<Divider />
+				<Text>500 Terry Francois St San Francisco, CA 94158</Text>
+				<Text>embeddedink22@gmail.com</Text>
+				<Text>4698769438</Text>
+			</VStack>
+			<ContactForm
+				inputBgColor={inputBgColor}
+				inputColor={inputColor}
+				formColor={formColor}
+			/>
+		</Stack>
+	);
 };
 export default ContactSection;
